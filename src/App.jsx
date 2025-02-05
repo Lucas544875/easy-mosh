68711492b70a30c2e70bf3926fec7ec2350449f2
takayanagi <tayanagi691@gmail.com>
2025-02-05 21:37:50 +0900

    [disable] 不要なスタイルを削除

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Header from '@features/header/Header';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-gray-900">
       <Header />
       <DatamoshEditor />
     </div>
