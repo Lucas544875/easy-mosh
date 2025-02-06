90113de6efe7bcaf9ad8f4a620b8a92ddf8efb77
takayanagi <tayanagi691@gmail.com>
2025-02-06 11:03:20 +0900

    [update] ロゴにもリンクを発行

--- src/components/features/header/Header.jsx
+++ src/components/features/header/Header.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import HeaderButton from './HeaderButton';
+import { Link } from 'react-router-dom';
 
 const Header = () => (
   <header className="flex justify-between items-center p-4 bg-slate-800 border-b text-white">
-    <div className="text-lg font-bold">
-      ロゴ
-    </div>
+    <Link to={"/"}>
+      <div className="text-lg font-bold">
+        ロゴ
+      </div>    
+    </Link>
     <nav className="flex space-x-4">
       <HeaderButton to={"/"}>編集</HeaderButton>
       <HeaderButton to={"/about"}>使い方</HeaderButton>
