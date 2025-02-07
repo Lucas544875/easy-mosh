1e58c060690d490de786558164601a95df69e003
takayanagi <tayanagi691@gmail.com>
2025-02-07 17:08:19 +0900

    [update] ライブラリが利用可能に #4

--- src/components/features/datamosh-editor/DatamoshEditor.jsx
+++ src/components/features/datamosh-editor/DatamoshEditor.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import SrcArea from '@features/src-area/SrcArea';
 import Preview from '@features/preview/Preview';
-import Timeline from '@features/timeline/Timeline';
+import TimelineEditor from '@features/timeline/Timeline';
 
 const DatamoshEditor = () => {
   const [videos, setVideos] = useState([]);
@@ -21,7 +21,7 @@ const DatamoshEditor = () => {
           </div>
           {/* Timeline Section */}
           <div className="h-1/2 p-4">
-            <Timeline videos={videos} />
+            <TimelineEditor videos={videos} />
           </div>
         </div>
       </div>
--- src/components/features/timeline/Timeline.jsx
+++ src/components/features/timeline/Timeline.jsx
@@ -1,19 +1,48 @@
 import React from "react";
+import { Timeline } from '@xzdarcy/react-timeline-editor';
 
-const Timeline = ({videos}) => (
+const mockData= [{
+    id: "0",
+    actions: [
+      {
+        id: "action00",
+        start: 0,
+        end: 2,
+        effectId: "effect0",
+      },
+    ],
+  },
+  {
+    id: "1",
+    actions: [
+      {
+        id: "action10",
+        start: 1.5,
+        end: 5,
+        effectId: "effect1",
+      }
+    ],
+}]
+
+const mockEffect= {
+  effect0: {
+    id: "effect0",
+    name: "効果0",
+  },
+  effect1: {
+    id: "effect1",
+    name: "効果1",
+  },
+};
+
+const TimelineEditor = ({videos}) => (
   <div className="space-y-2">
     <h2 className="text-xl font-semibold">タイムライン</h2>
-    <div className="flex space-x-2 border p-2 rounded-lg">
-      {videos.map((video, index) => (
-        <div
-          key={index}
-          className="bg-gray-200 p-2 rounded-md cursor-pointer hover:bg-gray-300"
-        >
-          {video.name}
-        </div>
-      ))}
-    </div>
+    <Timeline
+      editorData={mockData}
+      effects={mockEffect}
+    />
   </div> 
 )
 
-export default Timeline;
+export default TimelineEditor;
