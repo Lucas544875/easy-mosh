1b4725de0596f5c0c46eaa870c80cdbff30c6d43
takayanagi <tayanagi691@gmail.com>
2025-02-21 23:27:04 +0900

    [update] レンダリングモーダルのボタン制御 #25

--- src/components/features/render-button/RenderButton.jsx
+++ src/components/features/render-button/RenderButton.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import Button from "@common/button";
-import { Tooltip, Modal } from "antd";
+import MyButton from "@common/button";
+import { Tooltip, Modal, Button } from "antd";
 import { useAtom } from 'jotai';
 import { timelineAtom, sortTimeline } from '@atoms/atom';
 import { useFfmpeg } from "./useFfmpeg";
@@ -26,34 +26,51 @@ const RenderButton = () => {
 
   const handleOk = () => {
     setIsModalOpen(false);
+    videoRef.current.src = '';
+    console.log('ok');
   }
 
-  const handleCancel = () => {
+  const handleCancel = () => {}
+
+  const handleClose = () => {
     setIsModalOpen(false);
+    videoRef.current.src = '';
   }
 
   return(   
     <>
       <Tooltip title="整列してレンダリング">
-        <Button 
+        <MyButton
           onClick={handleRender}
           className={`bg-zinc-600 text-white absolute bottom-4 right-4`}
         >
           レンダリング
-        </Button>
+        </MyButton>
       </Tooltip>
       <Modal
         title="レンダリング中"
         visible={isModalOpen}
         onOk={handleOk}
-        onCancel={handleCancel}
+        // onCancel={handleCancel}
         className='modal'
+        closable={false}
+        footer={
+          <>
+            <Button key="back" onClick={handleClose}>
+              Cancel
+            </Button>
+            <Button key="submit" type="primary" onClick={handleOk} disabled={isProcessing}>
+              Download
+            </Button>
+          </>
+        }
       >
         <video
           className="bg-black result-video"
           ref = {videoRef}
           controls
         />
+        <p>レンダリング中…</p>
         <p ref = {messageRef}></p>
       </Modal>
     </>
