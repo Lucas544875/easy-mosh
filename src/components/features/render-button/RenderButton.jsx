42c268519e1eee8da4af8112bde8afa053d20f3f
takayanagi <tayanagi691@gmail.com>
2025-02-21 15:52:24 +0900

    [update] ツールチップの配置 #25

--- src/components/features/render-button/RenderButton.jsx
+++ src/components/features/render-button/RenderButton.jsx
@@ -1,13 +1,32 @@
 import React from "react";
 import Button from "@common/button";
 import { Tooltip } from "antd";
+import { useAtom } from 'jotai';
+import { timelineAtom } from '@atoms/atom';
+// import { useVideoMerger } from './useVideoMerger';
 
-const RenderButton = ({className}) => (
-  <Tooltip title="整列してレンダリング">
-    <Button className={`bg-zinc-600 text-white ${className}`}>
-      レンダリング
-    </Button>
-  </Tooltip>
-)
+const RenderButton = ({className}) => {
+  const [data, setData] = useAtom(timelineAtom);
+  // const { mergeVideos, outputUrl } = useVideoMerger();
+  
+  const handleRender = () => {
+    console.log(data);
+    const videoUrls = data[0].actions.map((action) => action.data.src);
+    console.log(videoUrls);
+    // mergeVideos(videoUrls);
+    // console.log(outputUrl);
+  }
+
+  return(   
+    <Tooltip title="整列してレンダリング">
+      <Button 
+        onClick={handleRender}
+        className={`bg-zinc-600 text-white ${className}`}
+      >
+        レンダリング
+      </Button>
+    </Tooltip>
+  )
+}
 
 export default RenderButton;
