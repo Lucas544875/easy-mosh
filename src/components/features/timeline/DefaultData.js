2e028159b5b574309be26aa4f8385b869c52cc51
takayanagi <tayanagi691@gmail.com>
2025-02-22 17:24:58 +0900

    [update] Isubのプレビュー #31

--- src/components/features/timeline/DefaultData.js
+++ src/components/features/timeline/DefaultData.js
@@ -34,4 +34,32 @@ export const effectList = {
       },
     },
   },
+  "I-substitute": {
+    id: 'I-substitute',
+    name: '動画',
+    source: {
+      enter: ({ action, engine, time, isPlaying }) => {
+        const src = (action).data.src;
+        const cripStart = (action).data.cripStart;
+        videoControl.enter({src, startTime: action.start, time, engine,isPlaying, cripStart });
+      },
+      update: ({ action, time }) => {
+        const cripStart = action.data.cripStart;
+        videoControl.update({startTime: action.start, time, cripStart });
+      },
+      leave: ({ action, time, isPlaying }) => {
+        const cripStart = action.data.cripStart;
+        videoControl.leave({startTime: action.start + action.data.cripStart, endTime: action.end, time, isPlaying, cripStart });
+      },
+      stop: ({ action, time, isPlaying }) => {
+        const cripStart = action.data.cripStart;
+        videoControl.stop({startTime: action.start, time, isPlaying, cripStart });
+      },
+      start: ({ action, engine, time, isPlaying }) => {
+        const src = (action).data.src;
+        const cripStart = action.data.cripStart;
+        videoControl.enter({src, startTime: action.start, time, engine,isPlaying, cripStart });
+      },
+    },
+  },
 };
