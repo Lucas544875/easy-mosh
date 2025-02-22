7db6f77392c35c1959cb65a2a4302a7df60dda9e
takayanagi <tayanagi691@gmail.com>
2025-02-22 16:10:04 +0900

    [update] Pmenu の実装 #31

--- src/components/features/src-area/Pmenu.jsx
+++ src/components/features/src-area/Pmenu.jsx
@@ -4,80 +4,87 @@ import { Slider } from 'antd';
 const formatTime = (seconds) => {
   const minutes = Math.floor(seconds / 60);
   const secs = Math.floor(seconds % 60);
-  return `${minutes}:${secs.toString().padStart(2, '0')}`;
+  const msecs = Math.floor((seconds - Math.floor(seconds)) * 100);
+  return `${minutes}:${secs.toString().padStart(2, '0')}.${msecs.toString().padStart(2, '0')}`;
 };
 
-const CImenu = ({videoSrc, name, setItemDuration, setItemData}) => {
-  const [rangeValues, setRangeValues] = useState([0, 0]);
-  const [duration, setDuration] = useState(100);
-  const startPreviewVideoRef = useRef(null);
-  const endPreviewVideoRef = useRef(null);
+const Pmenu = ({videoSrc, name, setItemDuration, setItemData}) => {
+  const [flameRangeValue, setFlameRangeValue] = useState(0);
+  const [dulationRangeValue, setDulationRangeValue] = useState(5);
+  const [videoDuration, setVideoDuration] = useState(100);
+  const PreviewVideoRef = useRef(null);
   
   const handleLoadedMetadata = (e) => {
-    const videoDuration = e.target.duration;
-    setDuration(videoDuration);
-    setRangeValues([0, videoDuration]);
+    setVideoDuration(e.target.duration);
   };
 
-  const handleSliderChange = (newValues) => {
-    setRangeValues(newValues);
-    setItemDuration(newValues[1] - newValues[0]);
+  const handleFlameSliderChange = (newValues) => {
+    setFlameRangeValue(newValues);
     setItemData({
       src: videoSrc,
       name: name,
-      cripStart: newValues[0],
-      cripEnd: newValues[1],
+      flameTime: newValues
     });
-    if (startPreviewVideoRef.current) {
-      startPreviewVideoRef.current.currentTime = newValues[0];
-    }
-    if (endPreviewVideoRef.current) {
-      endPreviewVideoRef.current.currentTime = newValues[1];
+    if (PreviewVideoRef.current) {
+      PreviewVideoRef.current.currentTime = newValues;
     }
   };
+
+  const handleDulationSliderChange = (newValues) => {
+    setDulationRangeValue(newValues);
+    setItemDuration(newValues);
+  }
   
   return (
     <>
       {/* プレビューエリア */}
-      <div className="flex justify-between">
+      <div className="flex justify-center">
         <video 
-          ref={startPreviewVideoRef}
+          ref={PreviewVideoRef}
           src={videoSrc}
           width="45%" 
           preload="metadata" 
           className='bg-black'
           onLoadedMetadata={handleLoadedMetadata}
         />
-        <video 
-          ref={endPreviewVideoRef}
-          src={videoSrc}
-          width="45%" 
-          preload="metadata" 
-          className='bg-black'
-        />
       </div>
-      {/* スライダー */}
+      {/* フレーム指定のスライダー */}
       <div className="w-full px-4">
         <Slider
-          range
           min={0}
-          max={duration}
-          step={0.03}
-          value={rangeValues}
-          onChange={handleSliderChange}
+          max={videoDuration}
+          step={0.01}
+          value={flameRangeValue}
+          onChange={handleFlameSliderChange}
           tipFormatter={(value) => formatTime(value)}
         />
       </div>
       <div>
-        <div className="mt-2 inline">
-          {formatTime(rangeValues[0])}~{formatTime(rangeValues[1])}
+        <div className="inline">
+          {formatTime(flameRangeValue)}
         </div>
         <div className="inline" style={{ color: '#bbb' }}>
-          /{formatTime(duration)}
+          /{formatTime(videoDuration)}
+        </div>
+      </div>
+      {/* 長さ指定のスライダー */}
+      <div className="w-full px-4">
+        <Slider
+          min={0}
+          max={10}
+          step={0.01}
+          value={dulationRangeValue}
+          onChange={handleDulationSliderChange}
+          tipFormatter={(value) => formatTime(value)}
+        />
+      </div>
+      <div>
+        <div className="inline">
+          {formatTime(dulationRangeValue)}
         </div>
       </div>
     </>
   );
 }
 
-export default CImenu;
+export default Pmenu;
