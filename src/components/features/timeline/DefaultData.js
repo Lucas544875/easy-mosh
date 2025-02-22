ee8c0e8031e1b935963e24d568ce48ed9c657617
takayanagi <tayanagi691@gmail.com>
2025-02-22 18:14:24 +0900

    [update] Pdupのプレビューも完成 #31

--- src/components/features/timeline/DefaultData.js
+++ src/components/features/timeline/DefaultData.js
@@ -62,4 +62,37 @@ export const effectList = {
       },
     },
   },
+  "P-duplicate": {
+    id: 'P-duplicate',
+    name: '動画',
+    source: {
+      enter: ({ action, engine, time, isPlaying }) => {
+        const src = (action).data.src;
+        // const cripStart = (action).data.cripStart;
+        const flameTime = (action).data.flameTime;
+        videoControl.enter({src, startTime: action.start, time, engine,isPlaying, cripStart:flameTime });
+      },
+      update: ({ action, time }) => {
+        // const cripStart = action.data.cripStart;
+        const flameTime = (action).data.flameTime;
+        videoControl.update({startTime: time, time, cripStart:flameTime });
+      },
+      leave: ({ action, time, isPlaying }) => {
+        // const cripStart = action.data.cripStart;
+        const flameTime = (action).data.flameTime;
+        videoControl.leave({startTime: time + action.data.cripStart, endTime: action.end, time, isPlaying, cripStart:flameTime });
+      },
+      stop: ({ action, time, isPlaying }) => {
+        // const cripStart = action.data.cripStart;
+        const flameTime = (action).data.flameTime;
+        videoControl.stop({startTime: time, time, isPlaying, cripStart:flameTime });
+      },
+      start: ({ action, engine, time, isPlaying }) => {
+        const src = (action).data.src;
+        // const cripStart = action.data.cripStart;
+        const flameTime = (action).data.flameTime;
+        videoControl.enter({src, startTime: time, time, engine,isPlaying, cripStart:flameTime });
+      },
+    },
+  },
 };
