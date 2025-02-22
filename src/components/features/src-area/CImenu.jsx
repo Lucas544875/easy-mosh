fad6a3766cecd4f0f2e7ea0b9aa9e4808a934318
takayanagi <tayanagi691@gmail.com>
2025-02-22 14:49:24 +0900

    [update] モーダルの中身を分割 #31

---
+++ src/components/features/src-area/CImenu.jsx
@@ -0,0 +1,79 @@
+import React, { useState, useRef } from "react";
+import { Modal, Slider, Radio } from 'antd';
+import { useAtom } from 'jotai';
+import { timelineAtom } from '@atoms/atom';
+
+const formatTime = (seconds) => {
+  const minutes = Math.floor(seconds / 60);
+  const secs = Math.floor(seconds % 60);
+  return `${minutes}:${secs.toString().padStart(2, '0')}`;
+};
+
+const CImenu = ({videoSrc, name}) => {
+  const [rangeValues, setRangeValues] = useState([0, 0]);
+  const [duration, setDuration] = useState(100);
+  const startPreviewVideoRef = useRef(null);
+  const endPreviewVideoRef = useRef(null);
+  const [timelineData, setTimelineData] = useAtom(timelineAtom);
+  
+  const handleLoadedMetadata = (e) => {
+    const videoDuration = e.target.duration;
+    setDuration(videoDuration);
+    setRangeValues([0, videoDuration]);
+  };
+
+  const handleSliderChange = (newValues) => {
+    setRangeValues(newValues);
+    if (startPreviewVideoRef.current) {
+      startPreviewVideoRef.current.currentTime = newValues[0];
+    }
+    if (endPreviewVideoRef.current) {
+      endPreviewVideoRef.current.currentTime = newValues[1];
+    }
+  };
+  
+  return (
+    <>
+      {/* プレビューエリア */}
+      <div className="flex justify-between">
+        <video 
+          ref={startPreviewVideoRef}
+          src={videoSrc}
+          width="45%" 
+          preload="metadata" 
+          className='bg-black'
+          onLoadedMetadata={handleLoadedMetadata}
+        />
+        <video 
+          ref={endPreviewVideoRef}
+          src={videoSrc}
+          width="45%" 
+          preload="metadata" 
+          className='bg-black'
+        />
+      </div>
+      {/* スライダー */}
+      <div className="w-full px-4">
+        <Slider
+          range
+          min={0}
+          max={duration}
+          step={0.1}
+          value={rangeValues}
+          onChange={handleSliderChange}
+          tipFormatter={(value) => formatTime(value)}
+        />
+      </div>
+      <div>
+        <div className="mt-2 inline">
+          {formatTime(rangeValues[0])}~{formatTime(rangeValues[1])}
+        </div>
+        <div className="inline" style={{ color: '#bbb' }}>
+          /{formatTime(duration)}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default CImenu;
--- src/components/features/src-area/SrcCardModal.jsx
+++ src/components/features/src-area/SrcCardModal.jsx
@@ -4,12 +4,7 @@ import MyButton from "@common/button";
 import "./modal.less";
 import { useAtom } from 'jotai';
 import { timelineAtom } from '@atoms/atom';
-
-const formatTime = (seconds) => {
-  const minutes = Math.floor(seconds / 60);
-  const secs = Math.floor(seconds % 60);
-  return `${minutes}:${secs.toString().padStart(2, '0')}`;
-};
+import CImenu from './CImenu';
 
 const options = [
   { label: 'copy', value: 'copy' },
@@ -17,66 +12,49 @@ const options = [
   { label: 'P-duplicate', value: 'P-duplicate' },
 ];
 
-const maxTime = (timeline) => {
-  const actions = timeline[0].actions;
-  let max = 0;
-  actions.forEach((action) => {
-    if (action.end > max) {
-      max = action.end;
-    }
-  });
-  return max;
-};
+// const maxTime = (timeline) => {
+//   const actions = timeline[0].actions;
+//   let max = 0;
+//   actions.forEach((action) => {
+//     if (action.end > max) {
+//       max = action.end;
+//     }
+//   });
+//   return max;
+// };
 
 const SrcCardModal = ({ videoSrc, name }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [rangeValues, setRangeValues] = useState([0, 0]);
-  const [duration, setDuration] = useState(100);
   const [radioValue, setRadioValue] = useState('copy');
-  const startPreviewVideoRef = useRef(null);
-  const endPreviewVideoRef = useRef(null);
-  const [timelineData, setTimelineData] = useAtom(timelineAtom);
+  const isPduplicate = radioValue === 'P-duplicate';
 
   const showModal = () => setIsModalOpen(true);
+
   const handleOk = () => {
-    const newTimelineItem = {
-      id: Date.now(),
-      start: maxTime(timelineData),
-      end: maxTime(timelineData) + rangeValues[1] - rangeValues[0],
-      effectId: radioValue, // 動画
-      flexible: false,
-      data: {
-        src: videoSrc,
-        name: name,
-        cripStart: rangeValues[0],
-        cripEnd: rangeValues[1],
-      },
-    }
-    // console.log(newTimelineItem);
-    const newTimelineData = [{
-      id: '1',
-      actions: timelineData[0].actions.concat(newTimelineItem)
-    }]
-    setTimelineData(newTimelineData);
+    // タイムラインへのアイテムの追加処理
+    // const newTimelineItem = {
+    //   id: Date.now(),
+    //   start: maxTime(timelineData),
+    //   end: maxTime(timelineData) + rangeValues[1] - rangeValues[0],
+    //   effectId: radioValue, // 動画
+    //   flexible: false,
+    //   data: {
+    //     src: videoSrc,
+    //     name: name,
+    //     cripStart: rangeValues[0],
+    //     cripEnd: rangeValues[1],
+    //   },
+    // }
+    // // console.log(newTimelineItem);
+    // const newTimelineData = [{
+    //   id: '1',
+    //   actions: timelineData[0].actions.concat(newTimelineItem)
+    // }]
+    // setTimelineData(newTimelineData);
     setIsModalOpen(false);
   };
+
   const handleCancel = () => setIsModalOpen(false);
-
-  const handleLoadedMetadata = (e) => {
-    const videoDuration = e.target.duration;
-    setDuration(videoDuration);
-    setRangeValues([0, videoDuration]);
-  };
-
-  const handleSliderChange = (newValues) => {
-    setRangeValues(newValues);
-    if (startPreviewVideoRef.current) {
-      startPreviewVideoRef.current.currentTime = newValues[0];
-    }
-    if (endPreviewVideoRef.current) {
-      endPreviewVideoRef.current.currentTime = newValues[1];
-    }
-  };
 
   return (
     <>
@@ -100,44 +78,7 @@ const SrcCardModal = ({ videoSrc, name }) => {
             buttonStyle="solid"
             onChange={(e) => setRadioValue(e.target.value)}
           />
-          {/* プレビューエリア */}
-          <div className="flex justify-between">
-            <video 
-              ref={startPreviewVideoRef}
-              src={videoSrc}
-              width="45%" 
-              preload="metadata" 
-              className='bg-black'
-              onLoadedMetadata={handleLoadedMetadata}
-            />
-            <video 
-              ref={endPreviewVideoRef}
-              src={videoSrc}
-              width="45%" 
-              preload="metadata" 
-              className='bg-black'
-            />
-          </div>
-          {/* スライダー */}
-          <div className="w-full px-4">
-            <Slider
-              range
-              min={0}
-              max={duration}
-              step={0.1}
-              value={rangeValues}
-              onChange={handleSliderChange}
-              tipFormatter={(value) => formatTime(value)}
-            />
-          </div>
-          <div>
-            <div className="mt-2 inline">
-              {formatTime(rangeValues[0])}~{formatTime(rangeValues[1])}
-            </div>
-            <div className="inline" style={{ color: '#bbb' }}>
-              /{formatTime(duration)}
-            </div>
-          </div>
+          {!isPduplicate && <CImenu videoSrc={videoSrc} name={name}/>}
         </div>
       </Modal>
     </>
