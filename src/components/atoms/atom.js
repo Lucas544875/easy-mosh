78789eb15fc7b6a881ba3e1bccbfeb31ec930b9a
takayanagi <tayanagi691@gmail.com>
2025-02-27 14:14:10 +0900

    [update] 初期データの削除

--- src/components/atoms/atom.js
+++ src/components/atoms/atom.js
@@ -5,57 +5,57 @@ const defaultData = [
   {
     id: '1',
     actions: [
-      {
-        id: Date.now()-1000,
-        start: 1,
-        end: 6,
-        effectId: 'copy', // 動画
-        flexible: false,
-        data: {
-          src: '/src/assets/mov.mp4',
-          name: '例1',
-          cripStart: 0,
-          cripEnd: 5
-        },
-      },
-      {
-        id: Date.now()-2000,
-        start: 10,
-        end: 15,
-        flexible: false,
-        effectId: 'copy', // 動画
-        data: {
-          src: '/src/assets/mov.mp4',
-          name: '例2',
-          cripStart: 0,
-          cripEnd: 5
-        },
-      },
-      {
-        id: Date.now()-3000,
-        start: 15,
-        end: 20,
-        flexible: false,
-        effectId: "P-duplicate",
-        data: {
-          src: '/src/assets/mov.mp4',
-          name: '例3',
-          flameTime: 0
-        }
-      },
-      {
-        id: Date.now()-4000,
-        start: 20,
-        end: 25,
-        flexible: false,
-        effectId: "I-substitute",
-        data: {
-          src: '/src/assets/mov.mp4',
-          name: '例4',
-          cripStart: 0,
-          cripEnd: 5
-        },
-      },
+      // {
+      //   id: Date.now()-1000,
+      //   start: 1,
+      //   end: 6,
+      //   effectId: 'copy', // 動画
+      //   flexible: false,
+      //   data: {
+      //     src: '/src/assets/mov.mp4',
+      //     name: '例1',
+      //     cripStart: 0,
+      //     cripEnd: 5
+      //   },
+      // },
+      // {
+      //   id: Date.now()-2000,
+      //   start: 10,
+      //   end: 15,
+      //   flexible: false,
+      //   effectId: 'copy', // 動画
+      //   data: {
+      //     src: '/src/assets/mov.mp4',
+      //     name: '例2',
+      //     cripStart: 0,
+      //     cripEnd: 5
+      //   },
+      // },
+      // {
+      //   id: Date.now()-3000,
+      //   start: 15,
+      //   end: 20,
+      //   flexible: false,
+      //   effectId: "P-duplicate",
+      //   data: {
+      //     src: '/src/assets/mov.mp4',
+      //     name: '例3',
+      //     flameTime: 0
+      //   }
+      // },
+      // {
+      //   id: Date.now()-4000,
+      //   start: 20,
+      //   end: 25,
+      //   flexible: false,
+      //   effectId: "I-substitute",
+      //   data: {
+      //     src: '/src/assets/mov.mp4',
+      //     name: '例4',
+      //     cripStart: 0,
+      //     cripEnd: 5
+      //   },
+      // },
     ],
   },
 ];
--- src/components/atoms/srcListAtom.js
+++ src/components/atoms/srcListAtom.js
@@ -1,11 +1,11 @@
 import { atom } from 'jotai';
 
 const defaultData = [
-  {
-    id: Date.now(),
-    name: "mov.mp4",
-    url: "/src/assets/mov.mp4",
-  }
+  // {
+  //   id: Date.now(),
+  //   name: "mov.mp4",
+  //   url: "/src/assets/mov.mp4",
+  // }
 ]
 
 export const srcListAtom = atom(defaultData);
