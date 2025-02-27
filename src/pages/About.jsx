eff098bc2ae6d96ed3df41fe3772f8f0f5baa3f6
takayanagi <tayanagi691@gmail.com>
2025-02-27 10:21:19 +0900

    [add] aboutに説明を適用 #3

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,10 +3,12 @@ import Header from '@features/header/Header';
 import Description from '@features/description/Description';
 
 const About = () => (
-  <div className="flex flex-col h-screen w-screen bg-zinc-900 text-white">
+  <div className="flex flex-col h-full w-screen bg-zinc-900 text-white">
     <Header />
-    <div className='p-4'>
-      <Description />
+    <div className='flex flex-row justify-center'>
+      <div className='lg:w-1/2'>
+        <Description />
+      </div>
     </div>
   </div>
 )
